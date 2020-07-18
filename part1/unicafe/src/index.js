b7aa86e861441dfa67d77f9e647aac51b91c7a62
yilin xu <xu.yilin@husky.neu.edu>
2020-07-18 14:41:17 -0400

    setup table in unicafe 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,39 +5,49 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
+const Statistic = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
 
-const Statistic = ({text,value})=>(
-<p>{text} {value}</p>
-)
-
-const Statistics = ({good, neutral, bad})=>{
-
+const Statistics = ({ good, neutral, bad }) => {
   let sum = good + neutral + bad;
 
-  if (sum > 0){
-    let average = (good - bad)/sum;
-    let positive = good / sum * 100;
-    return (<div>
-      <h2>statistics</h2>
-    <Statistic text = "good" value = {good}/>
-    <Statistic text = "neutral" value = {neutral}/>
-    <Statistic text = "bad" value = {bad}/>
-    <Statistic text = "all" value = {sum}/>
-    <Statistic text = "average" value = {average}/>
-    <Statistic text = "positive" value = {positive + " %"}/>
-    </div>
-    )
-  }else{
-    return (<div>
-      <h2>statistics</h2>
-      <p>No feedback given</p>
-    </div>)
+  if (sum > 0) {
+    let average = (good - bad) / sum;
+    let positive = (good / sum) * 100;
+    return (
+      <div>
+        <h2>statistics</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <Statistic text="good" value={good} />
+            <Statistic text="neutral" value={neutral} />
+            <Statistic text="bad" value={bad} />
+            <Statistic text="all" value={sum} />
+            <Statistic text="average" value={average} />
+            <Statistic text="positive" value={positive + " %"} />
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    );
   }
-
-  
 };
-
-
 
 const App = () => {
   // save clicks of each button to own state
@@ -51,7 +61,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="Good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
-      <Statistics good = {good} neutral = {neutral} bad = {bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
