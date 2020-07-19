c29d74a3b3f40b49905038675134a928e2fa2ed2
yilin xu <xu.yilin@husky.neu.edu>
2020-07-18 20:21:08 -0400

    ex2.3 update

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Total = ({ course }) => {
   const sum = course.parts.reduce((total, part) => part.exercises + total, 0);
-  return <p>Number of exercises {sum}</p>;
+  return <p style={{ fontWeight: "bold" }}>total of {sum} exercises</p>;
 };
 
 export default Total;
