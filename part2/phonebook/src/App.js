8540dec7687450a6214bb68782263f2cb4add0a4
yilin xu <xu.yilin@husky.neu.edu>
2020-07-18 21:12:42 -0400

    phonebook 2.6 update

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log(newName);
+    if (newName.length > 0) {
+      setPersons(persons.concat({ name: newName }));
+    }
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={handleNameChange} value={newName} />
+        </div>
+        <div>
+          <button onClick={addPerson} type="submit">
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+
+      <div>debug: {newName}</div>
+    </div>
+  );
+};
+
+export default App;
