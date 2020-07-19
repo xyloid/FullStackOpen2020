200480825dbf300dc2b7689ab166126f2d58eca0
yilin xu <xu.yilin@husky.neu.edu>
2020-07-18 20:24:40 -0400

    ex 2.4 update

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,30 +1,60 @@
-import React from 'react';
-import Course from './components/Course'
+import React from "react";
+import Course from "./components/Course";
 
 const App = () => {
-    const course = {
+  const courses = [
+    {
+      name: "Half Stack application development",
       id: 1,
-      name: 'Half Stack application development',
       parts: [
         {
-          name: 'Fundamentals of React',
+          name: "Fundamentals of React",
           exercises: 10,
-          id: 1
+          id: 1,
         },
         {
-          name: 'Using props to pass data',
+          name: "Using props to pass data",
           exercises: 7,
-          id: 2
+          id: 2,
         },
         {
-          name: 'State of a component',
+          name: "State of a component",
           exercises: 14,
-          id: 3
-        }
-      ]
-    }
-  
-    return <Course course={course} />
-  }
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  export default App
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
