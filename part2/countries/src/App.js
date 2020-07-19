8b5576a789fedf4d107505ee0dbd29945a69e48d
yilin xu <xu.yilin@husky.neu.edu>
2020-07-19 14:54:12 -0400

    add show/hide button

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,12 +3,44 @@ import axios from "axios";
 
 const maxCountries = 10;
 
+const Country = ({ country }) => {
+  const [isShow, setShow] = useState(false);
+
+  if (isShow) {
+    return (
+      <div>
+        <p key={country.name}>
+          {country.name}
+          <button onClick={() => setShow(!isShow)}>hide</button>
+        </p>
+
+        <h2>{country.name}</h2>
+        <p>captial {country.capital}</p>
+        <p>population {country.population}</p>
+        <h2>languages</h2>
+        <ul>
+          {country.languages.map((language) => (
+            <li key={language.name}>{language.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    console.log(country);
+    return (
+      <p key={country.name}>
+        {country.name} <button onClick={() => setShow(!isShow)}>show</button>
+      </p>
+    );
+  }
+};
+
 const Countries = ({ countries }) => {
   if (countries.length > maxCountries) {
     return <div>Too many matches, specify another filter.</div>;
   } else if (countries.length === 1) {
     let country = countries[0];
-    console.log("show single contry details",country);
+    console.log("show single contry details", country);
     return (
       <div>
         <h2>{country.name}</h2>
@@ -23,11 +55,12 @@ const Countries = ({ countries }) => {
       </div>
     );
   } else {
-    console.log("current country list",countries);
+    console.log("current country list", countries);
     return (
       <div>
         {countries.map((country) => (
-          <p key={country.name}>{country.name}</p>
+          // <p key={country.name}>{country.name}</p>
+          <Country key={country.name} country={country} />
         ))}
       </div>
     );
