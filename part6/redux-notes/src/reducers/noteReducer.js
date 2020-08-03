2b2543a157d06c8af93b1ee4288a921f0ab9e16c
yilin xu <xu.yilin@husky.neu.edu>
2020-08-02 22:03:08 -0400

    added toggle importance

--- part6/redux-notes/src/reducers/noteReducer.js
+++ part6/redux-notes/src/reducers/noteReducer.js
@@ -3,6 +3,20 @@ const noteReducer = (state = [], action) => {
     return state.concat(action.data);
   }
 
-  return state;
+  switch (action.type) {
+    case "NEW_NOTE":
+      return state.concat(action.data);
+    case "TOGGLE_IMPORTANCE": {
+      const id = action.data.id;
+      const noteToChange = state.find((n) => n.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      };
+      return state.map((note) => (note.id === id ? changedNote : note));
+    }
+    default:
+      return state;
+  }
 };
 export default noteReducer;
