5f284d68a0ea42aca4f01a6f7bb77367125afc11
yilin xu <xu.yilin@husky.neu.edu>
2020-08-03 16:43:18 -0400

    update part6-2

--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -6,20 +6,21 @@ import App from "./App";
 import noteReducer from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
 
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
+import { composeWithDevTools } from "redux-devtools-extension";
 
-
+import { createNote } from "./reducers/noteReducer";
+import { filterChange } from "./reducers/filterReducer";
 
 const reducer = combineReducers({ notes: noteReducer, filter: filterReducer });
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
-console.log(store.getState())
+console.log(store.getState());
 
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
+// store.subscribe(() => console.log(store.getState()));
+// store.dispatch(filterChange("IMPORTANT"));
+// store.dispatch(
+//   createNote("combineReducers forms one reducer from many simple reducers")
+// );
 
 ReactDOM.render(
   <Provider store={store}>
