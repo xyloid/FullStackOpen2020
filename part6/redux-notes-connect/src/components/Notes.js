e22e42f9c8a252e43178500d12d1091f8c8165f9
yilin xu <xu.yilin@husky.neu.edu>
2020-08-04 11:05:12 -0400

    get rid of useSelector

--- part6/redux-notes-connect/src/components/Notes.js
+++ part6/redux-notes-connect/src/components/Notes.js
@@ -15,27 +15,10 @@ const Note = ({ note, handleClick }) => {
 
 const Notes = (props) => {
   const dispatch = useDispatch();
-  // const notes = useSelector(({filter, notes}) => {
-  //   if (filter === "ALL") {
-  //     return notes;
-  //   }
-  //   return filter === "IMPORTANT"
-  //     ? notes.filter((note) => note.important)
-  //     : notes.filter((note) => !note.important);
-  // });
-
-  const notesToShow = () => {
-    if (props.filter === "ALL ") {
-      return props.notes;
-    }
-    return props.filter === "IMPORTANT"
-      ? props.notes.filter((note) => note.important)
-      : props.notes.filter((note) => !note.important);
-  };
 
   return (
     <ul>
-      {notesToShow().map((note) => (
+      {props.notes.map((note) => (
         <Note
           key={note.id}
           note={note}
@@ -47,11 +30,15 @@ const Notes = (props) => {
 };
 
 const mapStateToProps = (state) => {
+  if (state.filter === "ALL") {
+    return { notes: state.notes };
+  }
   return {
-    notes: state.notes,
-    filter: state.filter,
+    notes:
+      state.filter === "IMPORTANT"
+        ? state.notes.filter((note) => note.important)
+        : state.notes.filter((note) => !note.important),
   };
 };
-
 const ConnectedNotes = connect(mapStateToProps)(Notes);
 export default ConnectedNotes;
