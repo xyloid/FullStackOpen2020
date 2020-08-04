62ea8f08f1b7aa23a97eea085878da4abaa6324a
yilin xu <xu.yilin@husky.neu.edu>
2020-08-04 09:47:22 -0400

    setup for part6d

--- part6/redux-notes/src/App.js
+++ part6/redux-notes/src/App.js
@@ -2,15 +2,12 @@ import React, { useEffect } from "react";
 import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
 import VisibilityFilter from "./components/VisibilityFilter";
-import noteService from "./services/notes";
 import { initializeNotes } from "./reducers/noteReducer";
 import { useDispatch } from "react-redux";
 
 const App = () => {
   const dispatch = useDispatch();
-  useEffect(() => {
-    noteService.getAll().then((notes) => dispatch(initializeNotes(notes)));
-  }, [dispatch]);
+  useEffect(() => {    dispatch(initializeNotes())   },[dispatch]) 
 
   return (
     <div>
