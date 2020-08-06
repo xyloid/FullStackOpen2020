d2c39befa954c5bfec553c2917eeeb6f874ee4a1
yilin xu <xu.yilin@husky.neu.edu>
2020-08-06 15:06:01 -0400

    add connection to the backend

--- part7/class-anecdotes/src/App.js
+++ part7/class-anecdotes/src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { anecdotes: [], current: 0 };
   }
 
+  componentDidMount = () => {
+    axios.get("http://localhost:3001/anecdotes").then((response) => {
+      this.setState({ anecdotes: response.data });
+    });
+  };
+
   render() {
+    if (this.state.anecdotes.length === 0) {
+      return <div>no anecdotes...</div>;
+    }
+
     return (
       <div>
         <h1>anecdote of the day</h1>
+        <div>{this.state.anecdotes[this.state.current].content} </div>
+        <button>next</button>
       </div>
     );
   }
