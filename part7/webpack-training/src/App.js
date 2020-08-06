f15e431c0cca0e1fbc10df45db1537ebfbc6043f
yilin xu <xu.yilin@husky.neu.edu>
2020-08-06 12:46:57 -0400

    start class component

--- part7/webpack-training/src/App.js
+++ part7/webpack-training/src/App.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = () => <div className="container">hello webpack</div>;
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setNotes(response.data);
+    });
+  }, [url]);
+  return notes;
+};
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [values, setValues] = useState([]);
+  //   const url = 'https://blooming-atoll-75500.herokuapp.com/api/notes'
+  //   const notes = useNotes(url)
+  const notes = useNotes(BACKEND_URL);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+    setValues(values.concat(counter));
+  };
+
+  return (
+    <div className="container">
+      hello webpack {counter} clicks
+      <button onClick={handleClick}>press</button>
+      <div>
+        {notes.length} notes on server {BACKEND_URL}
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- part7/webpack-training/webpack.config.js
+++ part7/webpack-training/webpack.config.js
@@ -1,25 +1,39 @@
 const path = require("path");
+const webpack = require("webpack");
 
-const config = {
-  entry: ["@babel/polyfill", "./src/index.js"],
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "main.js",
-  },
-  devServer: {    
-      contentBase: path.resolve(__dirname, 'build'),    
-      compress: true,    
-      port: 3000,  
+const config = (env, argv) => {
+  console.log("argv", argv.mode);
+
+  const backend_url =
+    argv.mode === "production"
+      ? "https://blooming-atoll-75500.herokuapp.com/api/notes"
+      : "http://localhost:3001/notes";
+
+  return {
+    entry: ["@babel/polyfill", "./src/index.js"],
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "main.js",
     },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        query: { presets: ["@babel/preset-env", "@babel/preset-react"] },
-      },
-      { test: /\.css$/, loaders: ["style-loader", "css-loader"] },
+    devServer: {
+      contentBase: path.resolve(__dirname, "build"),
+      compress: true,
+      port: 3000,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          query: { presets: ["@babel/preset-env", "@babel/preset-react"] },
+        },
+        { test: /\.css$/, loaders: ["style-loader", "css-loader"] },
+      ],
+    },
+    devtool: "source-map",
+    plugins: [
+      new webpack.DefinePlugin({ BACKEND_URL: JSON.stringify(backend_url) }),
     ],
-  },
+  };
 };
 module.exports = config;
