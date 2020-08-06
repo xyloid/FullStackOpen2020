2e10943a4eaaa9f54081082d632c128ddf2c2188
yilin xu <xu.yilin@husky.neu.edu>
2020-08-06 14:54:54 -0400

    setup class components anecdotes

---
+++ part7/class-anecdotes/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>anecdote of the day</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
